8ee974c50e9634f7d67e9cd7ace71a8578306cc5
Kaushiki Sarkar <162082065+BUBBLY007@users.noreply.github.com>
2025-07-20 22:42:07 +0530

    Week5/Kafka & Zookeeper

---
+++ Week5_Kafka_Zookeeper/Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Confluent.Kafka;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            BootstrapServers = "localhost:9092",
+            GroupId = "chat-group",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+        string topic = "chat";
+        using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
+        {
+            consumer.Subscribe(topic);
+            Console.WriteLine("Listening for messages...");
+            while (true)
+            {
+                var msg = consumer.Consume();
+                Console.WriteLine($"Received: {msg.Message.Value}");
+            }
+        }
+    }
+}
+
---
+++ Week5_Kafka_Zookeeper/Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Confluent.Kafka;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var config = new ProducerConfig { BootstrapServers = "localhost:3030" };
+        string topic = "chat";
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            while (true)
+            {
+                Console.Write("You: ");
+                var input = Console.ReadLine();
+                if (input == "exit") break;
+                producer.Produce(topic, new Message<Null, string> { Value = input });
+            }
+        }
+    }
+}
+
